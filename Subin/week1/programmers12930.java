0d0329ac6186a056353a66ddb949550609cfc779
Subin <supiao0123@gmail.com>
2022-09-23 17:44:26 +0900

    Solved week 1 assignment(edited)

--- Subin/week1/programmers12930.java
+++ Subin/week1/programmers12930.java
@@ -1,34 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] separated = s.split("\\s+");
-        List<String> words = new ArrayList<>();
-        // String[] words = new String[];
-        for(int i = 0; i < separated.length; i++) {
-            for(int j = 0; j < (separated[i]).length(); j++) {
-                if((j % 2) == 0) {
-                    answer += Character.toString(separated[i].charAt(j)).toUpperCase();
+        int counter = -1;
+        for(int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+            if((s.charAt(i)) == ' ') {
+                counter = -1;
+                System.out.println(counter);
+                answer += s.charAt(i);
+            }
+            else {
+                counter += 1;
+            }
+            
+            if(counter != -1) {
+                if((counter % 2) == 0) {
+                    answer += Character.toUpperCase(s.charAt(i));
                 }
                 else {
-                    answer += Character.toString(separated[i].charAt(j)).toLowerCase();
+                    answer += Character.toLowerCase(s.charAt(i));
                 }
             }
-            words.add(answer);
-            // if(i == separated.length - 1)
-            //     return answer;
-            // else {
-            //     answer += " ";
-            // }
+
         }
-        
-        for(int x = 0; x < separated.length; x++) {
-            s = s.replace(separated[x], words.get(x));
-        }
-        answer = s;
-        
         return answer;
     }
 }
