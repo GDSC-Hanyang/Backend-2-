50b456b392d8feb66de883c1f138936bc0bc9ae8
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2022-10-09 18:14:38 +0900

    Add week3 assignments

---
+++ Taeyoon/week2/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,47 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    // 생성자 주입
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/CreateMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        //System.out.println("member.getName() = " + member.getName());
+
+        memberService.join(member);
+
+        // 회원 가입이 끝난 후 홈 화면으로 보내기
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // 키 "members", 값 members로 모델에 저장
+        return "members/memberList";
+    }
+}
